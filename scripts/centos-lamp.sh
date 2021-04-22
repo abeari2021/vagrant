cc7bd41f29e5d5dfa89d3abfc3c3d1141e3b4070
Abe Aripez <a.aripez@ibm,com>
2021-04-21 20:59:01 -0700

    Updated LAMP script with GitHub files

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -30,5 +30,8 @@ service mysqld start
 mysql -u root -e "SHOW DATABSES";
 
 # Download Starter Content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/abeari2021/vagrant/main/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/abeari2021/vagrant/main/files/info.php
 
 service httpd restart
